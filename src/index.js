f8de229149c3a8c3cebeceea5bc9502ebe722ba6
emmanuel <ehdz851@gmail.com>
2019-02-18 12:30:48 -0600

    Firebase

--- src/index.js
+++ src/index.js
@@ -3,8 +3,25 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {BrowserRouter} from 'react-router-dom'
+import firebase from 'firebase'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+firebase.initializeApp(
+    {
+        apiKey: "AIzaSyDveoaafaLOa1ztv_86-GQ-uyolwR2KWz0",
+        authDomain: "fir-ppf.firebaseapp.com",
+        databaseURL: "https://fir-ppf.firebaseio.com",
+        projectId: "fir-ppf",
+        storageBucket: "fir-ppf.appspot.com",
+        messagingSenderId: "404050777920"
+      }
+)
+
+ReactDOM.render(
+<BrowserRouter>
+<App />
+</BrowserRouter>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
